771e74eb8516a0d8e7581252ab49bc9849d45d59
aswinbennyofficial <aswinbenny.official@gmail.com>
2023-04-03 13:44:46 +0530

    updated file

--- Participation.py
+++ Participation.py
@@ -3,30 +3,30 @@ import json
 import psycopg2
 import pandas as pd
 
-
+def main():
 # import json file to here
-with open('config.json') as f:
-       data = json.load(f)
+       with open('config.json') as f:
+              data = json.load(f)
 
 
-# Connect to your postgres DB
-conn = psycopg2.connect(dbname=data["dbname"],user=data["user"],host=data["host"], password=data["password"],port=data["port"])
+       # Connect to your postgres DB
+       conn = psycopg2.connect(dbname=data["dbname"],user=data["user"],host=data["host"], password=data["password"],port=data["port"])
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
+       # Open a cursor to perform database operations
+       cur = conn.cursor()
 
-# Execute a query
-cur.execute("SELECT * FROM participation") 
+       # Execute a query
+       cur.execute("SELECT * FROM participation") 
 
-# Retrieve query results
-records = cur.fetchall()
-
-
-FieldTuple = [('user_id', 'event_id', 'id', 'created_at', 'updated_at', 'has_attended', 'has_attended_at','attendee_id')]
+       # Retrieve query results
+       records = cur.fetchall()
 
 
-df = pd.DataFrame(FieldTuple+records)
-df.to_csv('./out/participation_records.csv', index=False, header=False)
+       FieldTuple = [('user_id', 'event_id', 'id', 'created_at', 'updated_at', 'has_attended', 'has_attended_at','attendee_id')]
 
 
-conn.close()
+       df = pd.DataFrame(FieldTuple+records)
+       df.to_csv('./out/participation_records.csv', index=False, header=False)
+
+
+       conn.close()
